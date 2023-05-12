bd9e64a841a7227aa6a05f0150b25abe9ddc50eb
hyy <yh8953@utexas.edu>
2023-05-11 19:28:46 -0500

    update

--- leetcode/top-k-frequent-elements/solution.py
+++ leetcode/top-k-frequent-elements/solution.py
@@ -1,8 +1,11 @@
 class Solution:
     def topKFrequent(self, nums, k):
+        counts = Counter(nums)
         buckets = [[] for _ in range(len(nums) + 1)]
-        counts = Counter(nums).items()
-        for num, freq in counts:
+
+        for num, freq in counts.items():
             buckets[freq].append(num)
-        flat_list = [item for sublist in buckets for item in sublist]
+
+        flat_list = [num for bucket in buckets for num in bucket]
+
         return flat_list[::-1][:k]
