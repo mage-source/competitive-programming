06be40b9a4386c2bdc41e98acf7c1063463a3560
yiyuanh <yiyuanh@proton.me>
2023-10-24 10:22:04 -0500

    shorten 1873C solution

--- codeforces/1873/C.cpp
+++ codeforces/1873/C.cpp
@@ -1,47 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<vector<int>> target = {
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+  {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+  {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+  {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+  {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+  {1, 2, 3, 4, 4, 4, 4, 3, 2, 1},
+  {1, 2, 3, 3, 3, 3, 3, 3, 2, 1},
+  {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+};
+
 int solve() {
   vector<vector<char>> grid(10, vector<char>(10, '.'));
-  
+  int points = 0;
+
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       cin >> grid[i][j];
+
+      if (grid[i][j] == 'X') points += target[i][j];
     }
-  }
-
-  int left = 0, right = 10, top = 0, bot = 10;
-  int points = 0;
-  int cur_ring = 1;
-
-  while (left < right && top < bot) {
-    int cur_points = 0;
-    for (int i = left; i < right; i++) {
-      if (grid[top][i] == 'X') points += cur_ring; cur_points++;
-    }
-
-    top++;
-
-    for (int i = top; i < bot; i++) {
-      if (grid[i][right - 1] == 'X') points += cur_ring; cur_points++;
-    }
-
-    right--;
-
-    for (int i = right - 1; i >= left; i--) {
-      if (grid[bot - 1][i] == 'X') points += cur_ring; cur_points++;
-    }
-
-    bot--;
-
-    for (int i = bot - 1; i >= top; i--) {
-      if (grid[i][left] == 'X') points += cur_ring; cur_points++;
-    }
-
-    left++;
-
-    cur_ring++;
   }
 
   cout << points << endl;
