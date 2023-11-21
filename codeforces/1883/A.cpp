d9b2fa21e815d222aa7c8cb0fb02e42c896fdeb4
yiyuanh <yiyuanh@proton.me>
2023-11-21 15:14:06 -0600

    refactor 1883A

--- codeforces/1883/A.cpp
+++ codeforces/1883/A.cpp
@@ -2,34 +2,26 @@
 
 using namespace std;
 
-vector<vector<int>> costs = {
-  {0, 9, 8, 7, 6, 5, 4, 3, 2, 1},
-  {9, 0, 1, 2, 3, 4, 5, 6, 7, 8},
-  {8, 1, 0, 1, 2, 3, 4, 5, 6, 7},
-  {7, 2, 1, 0, 1, 2, 3, 4, 5, 6},
-  {6, 3, 2, 1, 0, 1, 2, 3, 4, 5},
-  {5, 4, 3, 2, 1, 0, 1, 2, 3, 4},
-  {4, 5, 4, 3, 2, 1, 0, 1, 2, 3},
-  {3, 6, 5, 4, 3, 2, 1, 0, 1, 2},
-  {2, 7, 6, 5, 4, 3, 2, 1, 0, 1},
-  {1, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-};
-
 void solve() {
   string s;
   cin >> s;
 
   int n = s.size();
-  int init_cost = costs[1][s[0] - '0'];
-  int move_cost = 0;
-
-  for (int i = 0; i < n - 1; i++) {
-    int src = s[i] - '0';
-    int dst = s[i + 1] - '0';
-    move_cost += costs[src][dst];
+  int cost;
+  
+  if (s[0] == '0') {
+    cost = 9;
+  } else {
+    cost = abs(1 - (s[0] - '0'));
   }
 
-  cout << 4 + init_cost + move_cost << endl;
+  for (int i = 0; i < n - 1; i++) {
+    int src = s[i] == '0' ? 10 : s[i] - '0';
+    int dst = s[i + 1] == '0' ? 10 : s[i + 1] - '0';
+    cost += abs(src - dst); 
+  }
+
+  cout << 4 + cost << endl;
 
   return;
 }
